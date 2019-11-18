01c6c61bacff5cf113edd1dfed18744956e2cf46
Mbare01 <vita.lorenzo@iisgalvanimi.edu.it>
2019-11-18 10:35:19 +0000

    Es 02.3 quasi finito

--- LEGO_PUG/app.js
+++ LEGO_PUG/app.js
@@ -1,14 +1,16 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const lego = require('./views/lego.json')
+const app = express();
 
 app.set('view engine', 'pug');   
-
 app.use(express.static(__dirname + '/public')); 
 
 app.get('/', function (req, res) {
-
- res.render('index');  
+    res.render('index', {
+        title: 'Homepage',
+        lego: lego.lego
+    });
 });
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
