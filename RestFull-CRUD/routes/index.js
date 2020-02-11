173ea9b199d24d2589ad224462ee9f4f50947303
Mbare01 <vita.lorenzo@iisgalvanimi.edu.it>
2020-02-11 11:59:35 +0000

    Es. 06.3.1 punto 1

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql');
+var createError = require('http-errors');
+
+const config = {
+  user: '4DD_22',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_22', //(Nome del DB)
+}
+
+let executeQuery = function (res, query, next) {
+  sql.connect(config, function (err) {
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    }
+    var request = new sql.Request(); // create Request object
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();
+        return;
+      }
+      res.send(result.recordset); //Il vettore con i dati è nel campo recordset (puoi loggare result per verificare)
+      sql.close();
+    });
+
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/images', function (req, res, next) {
+  let sqlQuery = "select Unit from dbo.[cr-unit-attributes]";
+  executeQuery(res, sqlQuery, next);
+  res.render("unita", {dati:result.recordset});
+});
+
 
 module.exports = router;
